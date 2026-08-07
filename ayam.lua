2f1936981737ee8007ed7772827c0748f6b395e2
luxxidroblox-code <luxxidroblox@gmail.com>
2026-08-07 10:22:32 +0700

    Update ayam.lua

--- ayam.lua
+++ ayam.lua
@@ -1,4 +1,4 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/X5Dermaster/RayField-Loader/refs/heads/main/rayfieldloader.lua'))()
 
 local DelayLabel, TeleportLabel, DestMinLabel, Dest5MinLabel
 local IncomeHourLabel, EarnedLabel, CurrentLabel, FpsLabel
